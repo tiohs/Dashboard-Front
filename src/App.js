ab576aca649f16fa1d3287eddf2999b683cbfe9a
tiohs <hamiltonsilva551@gmail.com>
2021-07-07 13:23:13 +0100

    Semantica header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { BrowserRouter } from 'react-router-dom';
 import Router from './routes';
-
+import Header from './components/Header/index';
 import GlobalStyle from './style/globals';
 function App() {
   return (
     <BrowserRouter>
       <GlobalStyle />
+      <Header />
       <Router />
     </BrowserRouter>
   );
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,27 @@
 import React from 'react';
 
 function Header() {
-  return <h1></h1>;
+  return (
+    <header>
+      <h2 class="header-title">
+        <label for="nav-toogle">
+          <span class="fas fa-bars"></span>
+        </label>
+        Dashboard
+      </h2>
+      <div class="search-wrapper">
+        <span class="fas fa-search"></span>
+        <input type="search" placeholder="Pesquisar" />
+      </div>
+      <div class="user-wrapper">
+        <img src="img/user.png" height="40px" width="40px" alt="Usuário" />
+        <div>
+          <h4>Simon Joseph</h4>
+          <small>Cliente</small>
+        </div>
+      </div>
+    </header>
+  );
 }
+
+export default Header;
