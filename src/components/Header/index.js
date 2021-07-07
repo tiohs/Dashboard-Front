19205ea7438c97ae6ba54fae54ea646ec87f5fc5
tiohs <hamiltonsilva551@gmail.com>
2021-07-07 13:37:52 +0100

    Mudar para Class Name

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,17 +5,17 @@ function Header() {
   return (
     <Conteiner>
       <header>
-        <h2 class="header-title">
+        <h2 className="header-title">
           <label for="nav-toogle">
-            <span class="fas fa-bars"></span>
+            <span className="fas fa-bars"></span>
           </label>
           Dashboard
         </h2>
-        <div class="search-wrapper">
-          <span class="fas fa-search"></span>
+        <div className="search-wrapper">
+          <span className="fas fa-search"></span>
           <input type="search" placeholder="Pesquisar" />
         </div>
-        <div class="user-wrapper">
+        <div className="user-wrapper">
           <img src="img/user.png" height="40px" width="40px" alt="Usuário" />
           <div>
             <h4>Simon Joseph</h4>
