c63e49406feb24e7938b68d0ec6a2457190945a9
tiohs <hamiltonsilva551@gmail.com>
2021-07-10 13:01:11 +0100

    delete store

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,34 +1,9 @@
 import React from 'react';
 
-import { FaSearch, FaUser } from 'react-icons/fa';
+// import { FaSearch, FaUser } from 'react-icons/fa';
 
 function Header() {
-  return (
-    <header>
-      <h2 className="header-title">
-        <label for="nav-toogle">
-          <span className="fas fa-bars"></span>
-        </label>
-        Dashboard
-      </h2>
-      <div className="search-wrapper">
-        <span>
-          <FaSearch />
-        </span>
-
-        <input type="search" placeholder="Pesquisar" />
-      </div>
-      <div className="user-wrapper">
-        <span>
-          <FaUser size={16} />
-        </span>
-        <div>
-          <h4>Simon Joseph</h4>
-          <small>Cliente</small>
-        </div>
-      </div>
-    </header>
-  );
+  return <h1>Ola mundos</h1>;
 }
 
 export default Header;
