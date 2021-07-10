4245f5136213823f7248d50695e343bd8b924283
tiohs <hamiltonsilva551@gmail.com>
2021-07-10 13:26:10 +0100

    ok

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
-
+import { Container } from './style';
 // import { FaSearch, FaUser } from 'react-icons/fa';
 
 function Header() {
-  return <h1>Ola mundos</h1>;
+  return (
+    <Container>
+      <header>
+        <div className="headerTitle"></div>
+        <div className="search">
+          <input type="text" />
+        </div>
+        <div className="avatar">
+          <img src="" alt="" />
+          <strong>Hamilton Silva</strong>
+          <span>admin</span>
+        </div>
+      </header>
+    </Container>
+  );
 }
 
 export default Header;
---
+++ src/components/Header/style.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  header {
+    background: #fff;
+    display: flex;
+    justify-content: space-between;
+    padding: 1rem 1.5rem;
+    box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.2);
+    position: fixed;
+    left: 345px;
+    width: calc(100% - 345px);
+    top: 0;
+    z-index: 100;
+    transition: left 300ms;
+    align-items: center;
+  }
+`;
