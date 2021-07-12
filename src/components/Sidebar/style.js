532a287f713a1dca849679b7d6cd4fd521bb4823
tiohs <hamiltonsilva551@gmail.com>
2021-07-12 08:11:44 +0100

    Obaz

--- src/components/Sidebar/style.js
+++ src/components/Sidebar/style.js
@@ -10,5 +10,21 @@ export const Container = styled.div`
     background: #2570ff;
     z-index: 100;
     transition: width 300ms;
+    .sidebar-brand {
+      height: 90px;
+      padding: 1rem 0rem 1rem 2rem;
+      color: #fff;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      span {
+        display: inline-block;
+        padding-right: 1rem;
+        margin-left: 10px;
+        font-size: 24px;
+      }
+    }
   }
 `;
+
+export const Menu = styled.nav``;
