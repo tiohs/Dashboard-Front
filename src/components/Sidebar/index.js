2dd7206fab26159bf8c3b295a080ccb81af47595
tiohs <hamiltonsilva551@gmail.com>
2021-07-12 15:56:07 +0100

    active new code

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 import {
   FaPeopleCarry,
@@ -11,39 +11,10 @@ import {
 import { Container, Item } from './style';
 
 function Sidebar() {
-  const [li1, setLi1] = useState(true);
-  const [li2, setLi2] = useState(false);
-  const [li3, setLi3] = useState(false);
-  const [li4, setLi4] = useState(false);
+  const location = useLocation();
+  const { pathname } = location;
+  const [, splitLocation] = pathname.split('/');
 
-  function allLiFalse() {
-    setLi1(false);
-    setLi2(false);
-    setLi3(false);
-    setLi4(false);
-  }
-  function handleClick(e) {
-    if (e === 'ds') {
-      allLiFalse();
-      setLi1(true);
-      return;
-    }
-    if (e === 'in') {
-      allLiFalse();
-      setLi2(true);
-      return;
-    }
-    if (e === 'cl') {
-      allLiFalse();
-      setLi3(true);
-      return;
-    }
-    if (e === 'fu') {
-      allLiFalse();
-      setLi4(true);
-      return;
-    }
-  }
   return (
     <Container>
       <div>
@@ -52,26 +23,26 @@ function Sidebar() {
       </div>
       <nav>
         <ul>
-          <Item active={li1} onClick={() => handleClick('ds')}>
+          <Item active={splitLocation === '' ? true : false}>
             <Link to="/">
               <FaIgloo size={24} />
               <span>Dashboard</span>
             </Link>
           </Item>
 
-          <Item active={li2} onClick={() => handleClick('in')}>
+          <Item active={splitLocation === 'inbox' ? true : false}>
             <Link to="/inbox">
               <FaInbox size={24} />
               <span>Inbox</span>
             </Link>
           </Item>
-          <Item active={li3} onClick={() => handleClick('cl')}>
+          <Item>
             <Link>
               <FaUsers size={24} />
               <span>Cliente</span>
             </Link>
           </Item>
-          <Item active={li4} onClick={() => handleClick('fu')}>
+          <Item>
             <Link>
               <FaUserTie size={24} />
               <span>Funcionários</span>
